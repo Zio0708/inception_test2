c7823f4b4208b70ff52c3a25cb3138a1e0bb4ab7
Zio0708 <jiho9478@naver.com>
2020-10-08 19:03:18 +0900

    버전 0.145 - > 카메라 파일 탐색 기능 수정

--- camera.py
+++ camera.py
@@ -3,4 +3,4 @@ import time
 def capture_pic():
     com_time = time.strftime("%m:%d:%H:%M", time.localtime(time.time()))
     camera = PiCamera()
-    camera.capture('/home/pi/'+com_time+'.jpg')
+    camera.capture('/home/pi/capture/'+com_time+'.jpg')
--- fileIO.py
+++ fileIO.py
@@ -1,3 +1,5 @@
+import os
+
 from retrain_run_inference import run_inference_on_image
 from PetInfo import Pet
 def filewrite(petInfo):
@@ -20,6 +22,20 @@ def fileopen():
         pet_infos.append(pet_info)
     f.close()
     return pet_infos
+
+def search_new_image():
+    files_Path = "/home/pi/capture/"  # 파일들이 들어있는 폴더
+    file_name_and_time_lst = []
+    # 해당 경로에 있는 파일들의 생성시간을 함께 리스트로 넣어줌.
+    for f_name in os.listdir(f"{files_Path}"):
+        written_time = os.path.getctime(f"{files_Path}{f_name}")
+        file_name_and_time_lst.append((f_name, written_time))
+    # 생성시간 역순으로 정렬하고,
+    sorted_file_lst = sorted(file_name_and_time_lst, key=lambda x: x[1], reverse=True)
+    # 가장 앞에 이는 놈을 넣어준다.
+    recent_file = sorted_file_lst[0]
+    recent_file_name = recent_file[0]
+    return recent_file_name
 
 #if __name__ == '__main__':
 #     filewrite()
